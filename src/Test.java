9e36d0907b352b0bd782dbdad1d16ba607e65309
Paul Ballot <paulivsa@googlemail.com>
2013-05-15 10:35:18 +0200

    Neue Szenarien in Test

--- src/Test.java
+++ src/Test.java
@@ -4,12 +4,40 @@ public class Test{
 		//1. Barby → Zerbst; zeitgleich Prödel → Barby
 		//Ziel: Kollision auf Südkurve
 
+		public class test
+{
+public void test1()
+{
+Zug zug1 = Steuerung.zugErstellen(8,2)
+Steuerung.blockfahren();
+Steuerung.blockfahren();
+Zug zug2 = Steuerung.zugErstellen(2,5)
+Steuerung.fahren();
+}
+
+
+		
 		//2. Barby → Barby
 		//Ziel: Kann der Zug wenden? Was passiert,wenn Ziel = Start
 
+		
+		public void test2()
+		{
+		Zug zug1 = Steuerung zugErstellen(2,2);
+        Stuerung.fahren();
+        }
+		
 		//3. Lindau → Prödel; zeitgleich Prödel → Lindau
 		//Ziel: Kollision auf „Nordkurve“
-
+        public void test2()
+		{
+		Zug zug1 = Steuerung.zugErstellen(17,12);
+        Steuerung.blockfahren();
+        Steuerung.blockfahren();
+        Steuerung.blockfahren();
+        Zug zug2 = Steuerung.zugErstellen(8,13);
+        Steuerung.fahren();
+        }
 		//4. Lindau → Prödel (Fahrstrecke)
 		//Ziel: Versuch,jede Weiche und jedes Signal zu verstellen. 
 /*
@@ -41,3 +69,10 @@ public class Test{
 	}
 
 }
+
+
+//<<<<<<< HEAD
+//=======
+
+}
+//>>>>>>> Test2
