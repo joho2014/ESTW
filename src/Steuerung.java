acc45a4f0f250d9c01ce980b9e22bff7608eb15f
Flo <floalkofer@web.de>
2013-05-15 10:21:22 +0200

    Bisheriger Stand auf Schulrechnern

--- src/Steuerung.java
+++ src/Steuerung.java
@@ -1,18 +1,50 @@
-public class Steuerung{
-	Steuerung(){}
-	
-	public Zug zugErstellen(int start, int ziel){
-	
-		return new Zug(start,ziel);
-	}
+/**
+ * Steuerung für den Zug
+ * 
+ * @author (Florian A., Tobias, Norbert, Fabio) 
+ * @version (02.05.2013)
+ */
+public class Steuerung
+{
+    
+    ;
 
-	public void blockFahren(){
+    /**
+     * Constructor for objects of class Steuerung
+     */
+    public 	Steuerung(){}
 
-	}
 
-	public void fahren(){
-
-		
-	}
-
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void blockFahren()
+    {
+        
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public Zug zugErstellen(int start, int ziel, String name)
+    {
+        return new Zug(start,ziel,name);
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void fahren()
+    {
+        
+    }
 }
--- src/Zug.java
+++ src/Zug.java
@@ -1,11 +1,93 @@
-public class Zug{
-	private int start;
-	private int ziel;
-	private int Position; //Gleisabschnitt
 
-	Zug(int start,int ziel){ //Konstruktor
+/**
+ * Ein Zug des Bahnhofs
+ * 
+ * @author Florian,Fabio,Tobi,Norbert 
+ * @version 02.05.2013
+ */
+public class Zug
+{
+    private int position,start,ziel;
+    private String name;
+    private int Weg;
+    
+    /**
+     * Erstellt einen Zug
+     */
+    public Zug(int s, int z, String n)
+    {
+        position = s;
+        start = s;
+        ziel = z;
+        name = n;
+        Weg = 3;
+    }
+    
+    // -------------------------------------------Get-Methoden---------------------------------------------
 
-	}
-
-	
+    /**
+     * getPosition: gibt Position zurück
+     * 
+     * @return     position
+     */
+    public int getPosition()
+    {
+        return position;
+    }
+    
+    /**
+     * getZiel: gibt Ziel zurück
+     * 
+     * @return     ziel 
+     */
+    public int getZiel()
+    {
+        return ziel;
+    }
+    
+    /**
+     * getStart: gibt Start zurück
+     * 
+     * @return     start
+     */
+    public int getStart()
+    {
+        return start;
+    }
+    
+    /**
+     * getName: gibt Name zurück
+     * 
+     * @return     name
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    //-------------------------------------------Set-Methoden---------------------------------------------
+    
+    /**
+     * setPosition: setzt Postion auf einen Parameter pneu
+     * 
+     * @param  pneu  neue Postion
+     */
+    public void setPosition(int pneu)
+    {
+        position = pneu;
+    }
+    
+    /**
+     * wegErstellen: setzt Weg, indem durch die Variablen mit bekannten Strecken abgeglichen wird
+     *               und  eine passende zurückgegeben wird
+     * @param st     zum abgleichen
+     *        zi      zum abgleichen
+     *        
+     * @return      Weg
+     */
+    public String wegErstellen(int st, int zi)
+    {
+        return null; //dummy
+    }
+    
 }
