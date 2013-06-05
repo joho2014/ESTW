ff5e3643dc337f34bf14354151133a40ad33e0ff
Tobi <tobias_d@gmx.de>
2013-06-05 10:32:23 +0200

    Implementierung aller HINwege (d.h. 6 fehlen noch)

--- src/Zug.java
+++ src/Zug.java
@@ -94,7 +94,38 @@ public class Zug
     //Barby : 3
     //Prödel : 4
     {
-        return null; //damits compiled
+        //Lindau - Zerbst:
+        List listLZ = new List(datenmodell.getWeiche(17));
+        Node node = list.getHead();
+        listLZ.add(getWeiche(18)); listLZ.add(getWeiche(29)); listLZ.add(getWeiche(19)); listLZ.add(getWeiche(20)); listLZ.add(getWeiche(24)); listLZ.add(getWeiche(25)); listLZ.add(getWeiche(26)); listLZ.add(getWeiche(7)); listLZ.add(getWeiche(6)); listLZ.add(getWeiche(27)); listLZ.add(getWeiche(5));
+        
+        //Lindau - Barby:
+        List listLB = new List(datenmodell.getWeiche(17));
+        Node node = list.getHead();
+        listLB.add(getWeiche(18)); listLB.add(getWeiche(29)); listLB.add(getWeiche(19)); listLB.add(getWeiche(20)); listLB.add(getWeiche(21)); listLB.add(getWeiche(22)); listLB.add(getWeiche(4)); listLB.add(getWeiche(28)); listLB.add(getWeiche(2));
+        
+        //Lindau - Prödel:
+        List listLP = new List(datenmodell.getWeiche(17));
+        Node node = list.getHead();
+        listLP.add(getWeiche(18)); listLP.add(getWeiche(29)); listLP.add(getWeiche(23)); listLP.add(getWeiche(24)); listLP.add(getWeiche(25)); listLP.add(getWeiche(26)); listLP.add(getWeiche(12)); 
+        
+        //Zerbst - Barby:
+        List listZB = new List(datenmodell.getWeiche(9));
+        Node node = list.getHead();
+        listZB.add(getWeiche(27)); listZB.add(getWeiche(1)); listZB.add(getWeiche(2));
+        
+        //Zerbst - Prödel:
+        List listZP = new List(datenmodell.getWeiche(9));
+        Node node = list.getHead();
+        listZP.add(getWeiche(10)); listZP.add(getWeiche(11)); listZP.add(getWeiche(26)); listZP.add(getWeiche(12));
+        
+        //Prödel - Barby:
+        List listPB = new List(datenmodell.getWeiche(8));
+        Node node = list.getHead();
+        listPB.add(getWeiche(7)); listPB.add(getWeiche(6)); listPB.add(getWeiche(27)); listPB.add(getWeiche(1)); listPB.add(getWeiche(2));
+       
+        
+        return null;
     }
     
 }
