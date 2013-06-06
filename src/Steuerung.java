302a234af1197a30a011e58d02be58f22d14ad3c
Flo <floalkofer@web.de>
2013-06-06 09:30:35 +0200

    BlockFahren Update

--- src/Steuerung.java
+++ src/Steuerung.java
@@ -6,48 +6,53 @@
  */
 public class Steuerung
 {
-	
-	private List zuglist;
-	private DieGrafik anzeige;
-	private Datenmodell datenmodell;
+    
+    private List zuglist;
+    private DieGrafik anzeige;
+    private Datenmodell datenmodell;
 
-	/**
-	 * Constructor for objects of class Steuerung
-	 */
-	public Steuerung()
-	{
-		datenmodell = new Datenmodell();
-		anzeige = new DieGrafik(datenmodell,this);
-	}
+    /**
+     * Constructor for objects of class Steuerung
+     */
+    public Steuerung()
+    {
+        datenmodell = new Datenmodell();
+        anzeige = new DieGrafik(datenmodell,this);
+    }
 
 
-	/**
-	 * Jeder Zug wird auf den nächsten Gleisabschnitt bewegt, falls möglich
-	 *
-	 */
-	public void blockFahren()
-	{
-		
-	}
-	
-	/**
-	 * Erstellt einen Zug
-	 * 
-	 * @param   start       Legt den Startbahnhof fest
-	 *          ziel        Legt den Zielbahnhof fest
-	 *          name        Legt den Namen des Zuges fest
-	 * @return     Einen Zug
-	 */
-	public Zug zugErstellen(int start, int ziel, String name)
-	{
-		return new Zug(start,ziel,name, datenmodell);
-	}
-	
-	/**
-	 * Züge fahren komplette Strecke
-	 */
-	public void fahren()
-	{
-		
-	}
+    /**
+     * Jeder Zug wird auf den nächsten Gleisabschnitt bewegt, falls möglich
+     *
+     */
+    public void blockFahren()   
+    {
+        Node x = zuglist.getHead();
+        while (x != null){
+            //x.getDatum().fahrEinsWeiter();
+            x=x.next();
+          }
+          
+    }
+    
+    /**
+     * Erstellt einen Zug
+     * 
+     * @param   start       Legt den Startbahnhof fest
+     *          ziel        Legt den Zielbahnhof fest
+     *          name        Legt den Namen des Zuges fest
+     * @return     Einen Zug
+     */
+    public Zug zugErstellen(int start, int ziel, String name)
+    {
+        return new Zug(start,ziel,name, datenmodell);
+    }
+    
+    /**
+     * Züge fahren komplette Strecke
+     */
+    public void fahren()
+    {
+        
+    }
 }
