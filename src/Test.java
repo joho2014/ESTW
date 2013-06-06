2ddabb4e5f6e29873354b395b7e04f71839d2d0f
Johannes <dw0nk@hotmail.com>
2013-06-06 09:08:58 +0200

    Test final v1

--- src/Test.java
+++ src/Test.java
@@ -1,83 +1,86 @@
 public class Test{
-	private Steuerung st;
-	private Datenmodell d;
-	private DieGrafik g;
+    private Steuerung st;
+    private Datenmodell d;
+    private DieGrafik g;
 
-	/*
-	1. Barby --> Zerbst; zeitgleich Proedel --> Barby
-	Ziel: Kollision auf Suedkurve
-	*/
+    /**
+     *Barby --> Zerbst; zeitgleich Proedel --> Barby
+     *Ziel: Kollision auf Suedkurve
+     */
 
-	public void test1(){
-		Zug zug1 = st.zugErstellen(8, 2, "Zug 1");
-		st.blockFahren();
-		st.blockFahren();
-		Zug zug2 = st.zugErstellen(2, 5, "Zug 2");
-		g.textAusgeben("Position Zug1: "+Integer.toString(zug1.getPosition())+"\n"+"Position Zug2: "+Integer.toString(zug2.getPosition()));
-		st.fahren();
-	}
+    public void test1(){
+        Zug zug1 = st.zugErstellen(8, 2, "Zug 1");
+        st.blockFahren();
+        st.blockFahren();
+        Zug zug2 = st.zugErstellen(2, 5, "Zug 2");
+        g.textAusgeben("Position Zug1: "+Integer.toString(zug1.getPosition())+"\n"+"Position Zug2: "+Integer.   toString(zug2.getPosition()));
+        st.fahren();
+    }
 
-	/*
-	2. Barby --> Barby
-	Ziel: Kann der Zug wenden? Was passiert,wenn Ziel = Start
-	*/
-	public void test2(){
-		Zug zug1 = st.zugErstellen(2, 2, "Zug 1");
-		st.fahren();
-	}
+    /**
+     *Barby --> Barby
+     *Ziel: Kann der Zug wenden? Was passiert,wenn Ziel = Start
+     */
+    public void test2(){
+        Zug zug1 = st.zugErstellen(2, 2, "Zug 1");
+        st.fahren();
+    }
 
-	/*
-	3. Lindau --> Proedel; zeitgleich Proedel --> Lindau
-	Ziel: Kollision auf Nordkurve“
-	*/
-	public void test3(){
-		Zug zug1 = st.zugErstellen(17, 12, "Zug 1");
-		st.blockFahren();
-		st.blockFahren();
-		st.blockFahren();
-		Zug zug2 = st.zugErstellen(8, 13, "Zug 2");
-		st.fahren();
-	}
+    /**
+     *Lindau --> Proedel; zeitgleich Proedel --> Lindau
+     *Ziel: Kollision auf Nordkurve“
+     */
+    public void test3(){
+        Zug zug1 = st.zugErstellen(17, 12, "Zug 1");
+        st.blockFahren();
+        st.blockFahren();
+        st.blockFahren();
+        Zug zug2 = st.zugErstellen(8, 13, "Zug 2");
+        st.fahren();
+    }
 
-	/*
-	4. Lindau --> Proedel (Fahrstrecke)
-	Ziel: Versuch,jede Weiche und jedes Signal zu verstellen. 
-	*/
-	public void test4(){
-		Zug zug0 = st.zugErstellen(17, 12, "Zug 0");
-		st.blockFahren();
-		//d.s21.setStellung(true);
-		//d.w17.setStellung(false);
-		st.blockFahren();
-		//d.s18.setStellung(true);
-		//d.w39.setStellung(false);
-		st.blockFahren();
-		st.fahren();
-	}
+    /**
+     *Lindau --> Proedel (Fahrstrecke)
+    Ziel: Versuch,jede Weiche und jedes Signal zu verstellen. 
+     */
+    public void test4(){
+        Zug zug0 = st.zugErstellen(17, 12, "Zug 0");
+        st.blockFahren();
+        d.getSignal(21).setStellung(true);
+        d.getWeiche(17).setStellung(false);
+        st.blockFahren();
+        d.getSignal(18).setStellung(true);
+        d.getWeiche(39).setStellung(false);
+        st.blockFahren();
+        st.fahren();
+    }
 
-	/*
-	5. Zerbst --> Lindau; Lindau --> Proedel (Beides Fahrstrecken)
-	Ziel:Erschaffen zweier sich schneidender Fahrstrecken 
-	*/
-	public void test5(){
-		Zug zug1 = st.zugErstellen(5, 13, "Zug 1");
-		Zug zug2 = st.zugErstellen(17, 9, "Zug 2"); 
-	}
+    /**
+     *Zerbst --> Lindau; Lindau --> Proedel (Beides Fahrstrecken)
+     *Ziel:Erschaffen zweier sich schneidender Fahrstrecken 
+     */
+    public void test5(){
+        Zug zug1 = st.zugErstellen(5, 13, "Zug 1");
+        Zug zug2 = st.zugErstellen(17, 9, "Zug 2"); 
+    }
 
-	/*
-	6. Barby --> Lindau; Barby --> Lindau
-	Ziel:Zwei Zuege zeitgleich auf der selben Position.
-	*/
-	public void test6(){
-		Zug zug1  = st.zugErstellen(2, 13, "Zug 1");
-		Zug zug2  = st.zugErstellen(2, 13, "Zug 2");
-		st.fahren();
-	}
+    /**
+     *Barby --> Lindau; Barby --> Lindau
+     *Ziel:Zwei Zuege zeitgleich auf der selben Position.
+     */
+    public void test6(){
+        Zug zug1  = st.zugErstellen(2, 13, "Zug 1");
+        Zug zug2  = st.zugErstellen(2, 13, "Zug 2");
+        st.fahren();
+    }
 
-	public void test7() // Fahrt gegen die Fahrrichtung
-	{
-		Zug zug1 = st.zugErstellen(12, 9, "Zug 1");
-		st.fahren();
-	}
+    /**
+     *Fahrt gegen die Fahrrichtung
+     */
+    public void test7()
+    {
+        Zug zug1 = st.zugErstellen(12, 9, "Zug 1");
+        st.fahren();
+    }
 
 }
