0e121ca3bbeabe10c27167e0e9d8ec333b04d0f2
Johannes <mulandoo@gmail.com>
2013-06-20 09:21:37 +0200

    test methoden debug option in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main
 {
-	public static void main(String[] s) throws Exception{
-		Steuerung st = new Steuerung();
-	}	
+    public static void main(String[] s) throws Exception{
+       Steuerung st = new Steuerung();
+        if (s!=null){
+            st.test.test1();
+            st.test.test2();
+            st.test.test3();
+            st.test.test4();
+            st.test.test5();
+            st.test.test6();
+            st.test.test7();
+            st.test.test8();
+            st.test.test9();}
+
+    }   
 }
