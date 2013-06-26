09c7bf118c426c951a493df36fef01063fea378b
Norbert <No.Huber@web.de>
2013-06-26 10:27:25 +0200

    Grundstruktur ZugErstellen()

--- src/Steuerung.java
+++ src/Steuerung.java
@@ -1,107 +1,29 @@
+
 /**
- * Steuerung für den Zug
+ * Zweite Steuerung die mit nur einem Zug und Fahrstraßen arbeitet.
  * 
- * @author (Florian A., Tobias, Norbert, Fabio) 
- * @version (02.05.2013)
+ * @author Huber Norbert 
+ * @version 26.06.2013
  */
 public class Steuerung
 {
-    
-    private List trains;
-    private DieGrafik anzeige;
-    private Datenmodell datenmodell;
-    private Test test;
-    
+    private ZugNeu zug;
+    private Datenmodell dm;
+    private DieGrafik az;
+    public Steuerung()
+    {
+        
+    }
 
     /**
-     * Constructor for objects of class Steuerung
-     */
-    public Steuerung(){
-        trains = new List();
-        datenmodell = new Datenmodell();
-        anzeige = new DieGrafik(datenmodell,this);
-        test = new Test(this,datenmodell,anzeige);
-        figureSignals();
-        anzeige.update();
-    }
-    
-    /**
-     * Gibt das Objekt Test zurück
+     * An example of a method - replace this comment with your own
      * 
-     * @return     Objekt der Klasse Test
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
      */
-    public Test getTest(){
-        return test;  
-    }
-    
-    /**
-     * Jeder Zug wird auf den nächsten Gleisabschnitt bewegt, falls möglich
-     *
-     */
-    public void blockFahren(){
-        Node node = trains.getHead();
-        while(node != null){
-            Zug t = (Zug)node.getDatum();
-            if (t.nextTrack()) cleanup(t);
-            node = node.next();
-        }
-        anzeige.update();
-        System.out.println(trains);
-    }
-    
-    /**
-     * Erstellt einen Zug
-     * 
-     * @param   start       Legt den Startbahnhof fest
-     *          ziel        Legt den Zielbahnhof fest
-     *          name        Legt den Namen des Zuges fest
-     * @return     Einen Zug
-     */
-    public Zug zugErstellen(int start, int ziel, String name){
-        Zug n = new Zug(start,ziel,name, datenmodell);
-        trains.append(n);
-        wiederSperren();
-        figureSignals();
-        anzeige.update();
-        return n;
-    }
-    
-    /**
-     * Lässt die Züge in der Abhängigkeit von der Länge der Gleise weiterfahren
-     */
-    public void fahren(){}
-
-    /**
-     * Löscht einen bestimmten Zug und entsperrt von diesem gesperrte Signale und Weichen
-     * 
-     * @param z Der zu löschende Zug
-     */
-    public void cleanup(Zug z){
-        z.wegFreimachen();
-        trains.delete(z);
-        wiederSperren();
-        figureSignals();
-    }
-
-    /**
-     * Ändert Farbe und Sperrer des Signals
-     */
-    public void figureSignals(){
-        int i = 0;
-        while(datenmodell.getSignal(i) != null){
-            datenmodell.getSignal(i).figureState();
-            i++;
-        }
-    }
-
-    /**
-     * Sperrt Weichen und Signale für den ersten Zug
-     */
-    public void wiederSperren(){
-        Node node = trains.getHead();
-        while(node != null){
-            ((Zug)node.getDatum()).reservieren();
-            node = node.next();
-        }
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
     }
 }
--- src/Zug.java
+++ src/Zug.java
@@ -1,181 +1,38 @@
+
+/**
+ * Write a description of class ZugNeu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class Zug
 {
-    private String name;
-    private List weg;
-    private Datenmodell dm;
-    private Gleisstueck position;
-    
-    /**
-     * Konstruktor für Züge
-     * 
-     * @param   s        Legt den Startbahnhof fest
-     *          z        Legt den Zielbahnhof fest
-     *          n        Legt den Namen des Zuges fest
-     *          d        Erstellt ein Datenmodel
-     */
-    public Zug(int s, int z, String n,Datenmodell d)
+    public Zug()
     {
-        dm = d;
-        name = n;
-        weg = wegErstellen(s, z);
-        position = (Gleisstueck)weg.head();
+        // initialise instance variables
+        x = 0;
     }
 
-    /**
-     * Gibt Position zurück
-     * 
-     * @return     Position des Zuges
-     */
-    public Gleisstueck getPosition()
-    {
-        return position;
-    }
-
-    /**
-     * Gibt Name zurück
-     *
-     * @return     Name des Zuges
-     */
-    public String getName()
-    {
-        return name;
-    }
-
-    /**
-     * Bewegt Zug auf nächstes Gleisstueck des Weges
-     * 
-     * @return     Wahrheitswert, ob Bewegung des Zugs erfolgreich war
-     */
-    public boolean nextTrack(){
-        Gleisstueck next = (Gleisstueck)((Node)((Node)(weg.find(position))).next()).getDatum();
-        if(next == null){
-            position.verlassen(this);
-            return true;
+    public void ZugErstellen(){
+        if(start == "Zerbst"){
+            if(){}
+            else if(){}
+            else if(){}
         }
-        if(null != null){
-        	System.out.println("++++++++++++++++++++++++++++++++");
-        	System.out.println(weg);
-            System.out.println(next);
-            System.out.println(position);
-            System.out.println(position.verbindung(next));
-            System.out.println(position.verbindung(next).befahrbar(position, next, this));
-            System.out.println(next.getSperrer());
-            System.out.println("--------------------------------");
+        else if(start == "Lindau"){
+            if(){}
+            else if(){}
+            else if(){}
         }
-        if(next.befahrbar(this) && (position == null || position.verbindung(next).befahrbar(position, next, this))){
-            next.befahren(this);
-            if(position != null)position.verlassen(this);
-            position = next;
+        else if(start == "Barby"){
+            if(){}
+            else if(){}
+            else if(){}
         }
-        return false;
-    }
-    
-    /**
-     * Erstellt einen Weg
-     * 
-     * @param   s      Legt den Startbahnhof fest
-     *          z      Legt den Zielbahnhof fest
-     * @return     Eine Wegliste, die aus den zu befahrenden Gleisstuecken besteht
-     */
-    public List wegErstellen(int s, int z){
-        List nR = new List();
-        switch(s){
-            case 0: switch(z){
-                        case 1: int[] trs1 = {17, 18, 29, 19, 20, 24, 25, 26, 7, 6, 27, 5}; nR = buildTracks(trs1); break;
-                        case 2: int[] trs2 = {17, 18, 29, 19, 20, 21, 22, 4, 28, 2}; nR = buildTracks(trs2); break;
-                        case 3: int[] trs3 = {17, 18, 29, 23, 24, 25, 26, 12}; nR = buildTracks(trs3); break;
-                    } break;
-            case 1: switch(z){
-                        case 0: int[] trs4 = {9, 27, 1, 28, 3, 16, 15, 14, 13}; nR = buildTracks(trs4); break;
-                        case 2: int[] trs5 = {9, 27, 1, 2}; nR = buildTracks(trs5); break;
-                        case 3: int[] trs6 = {9, 10, 11, 26, 12}; nR = buildTracks(trs6); break;
-                    } break;
-            case 2: switch(z){
-                        case 0: int[] trs7 = {2, 28, 3, 16, 15, 14, 13}; nR = buildTracks(trs7); break;
-                        case 1: int[] trs8 = {2, 28, 3, 22, 25, 26, 7, 6, 27, 5}; nR = buildTracks(trs8); break;
-                        case 3: int[] trs9 = {2, 28, 3, 22, 25, 26, 12}; nR = buildTracks(trs9); break;
-                    } break;
-            case 3: switch(z){
-                        case 0: int[] trs10 = {8, 7, 6, 1, 28, 3, 16, 15, 14, 13}; nR = buildTracks(trs10); break;
-                        case 1: int[] trs11 = {8, 7, 27, 5}; nR = buildTracks(trs11); break;
-                        case 2: int[] trs12 = {8, 7, 6, 27, 1, 2}; nR = buildTracks(trs12); break;
-                    } break;
-        }
-        return nR;
-    }
-
-    /**
-     * Verkürzung der getGleisstueck Methode, um Code kürzer zu machen
-     * 
-     * @param   gleisstueck     Gleisstueck, dass aus Datenmodell geholt werden soll
-     * @return     Gleisstueck
-     */
-    public Gleisstueck g(int gleisstueck){
-        return dm.getGleisstueck(--gleisstueck);
-    }
-
-    /**
-     * Hilfsmethode für wegErstellen
-     * 
-     * @param   trs     int Array der Nummer der Gleisstuecke enthält
-     * @return     Eine Liste aus ints
-     */
-    public List buildTracks(int[] trs){
-        List l = new List();
-        l.add(null);
-        for(int i = 0; i < trs.length; i++){
-            l.append(g(trs[i]));
-        }
-        l.append(null);
-        return l;
-    }
-    
-    /**
-     * toString Methode
-     *
-     * @return     String
-     */
-    public String toString(){
-        if(position != null) return name + ": " + position.toString();
-        else return name + ": null";
-    }
-
-    /**
-     * Entsperrt Gleis
-     * 
-     */
-    public void wegFreimachen(){
-        Node node = weg.getHead();
-        Weiche last = null;
-        while(node != null){
-            if(node.getDatum() != null){
-                Gleisstueck gleis = ((Gleisstueck)node.getDatum());
-                gleis.entsperren(this);
-                if(gleis.nextW(last) != null) gleis.nextW(last).entsperren(this);
-                last = gleis.nextW(last);
-            }
-            node = node.next();
-        }
-    }
-
-    /**
-     * Sperrt ein zu befahrendes Gleisstueck
-     * 
-     */
-    public void reservieren(){
-        Node node = weg.getHead();
-        Weiche last = null;
-        while(node != null){
-            if(node.getDatum() != null){
-                Gleisstueck gleis = ((Gleisstueck)node.getDatum());
-                gleis.sperren(this);
-                if(gleis.nextW(last) != null){
-                    gleis.nextW(last).sperren(this);
-                    gleis.nextW(last).stellen(gleis, (Gleisstueck)(node.next().getDatum()), this);
-                }
-                last = gleis.nextW(last);
-            }
-            node = node.next();
+        else if(start == "Prödel"){
+            if(){}
+            else if(){}
+            else if(){}
         }
     }
 }
